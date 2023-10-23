91a432e281b1e61be7a9bc3edcb71398ac32fb02
Simion-Gheorghe-Leontino Inișconi <simioninisconi@gmail.com>
2023-10-23 20:16:04 +0300

    started adding new models

---
+++ MetalVerseBackend/Models/Announcement.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+public class Announcement
+{
+    public Guid id {  get; set; }
+
+}
---
+++ MetalVerseBackend/Models/Stream.cs
@@ -0,0 +1,10 @@
+﻿namespace MetalVerseBackend.Models
+{
+    public class Stream
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+        public string Image { get; set; }
+        public required string ApiLink { get; set; }
+    }
+}
