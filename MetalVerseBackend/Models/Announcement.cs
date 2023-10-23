5410a752eb573954ff39531173d3ad5e130bfde9
malinapavel <malina.pavel@student.upt.ro>
2023-10-23 22:50:45 +0300

    Oh boy...

---
+++ MetalVerseBackend/Models/Announcement.cs
@@ -0,0 +1,8 @@
+﻿public class Announcement
+{
+    public Guid Id {  get; set; }
+    public required Guid UserId { get; set; }
+    public required string Title { get; set; }
+    public string Description { get; set; }
+    public string? Image { get; set; }
+}
---
+++ MetalVerseBackend/Models/Inbox.cs
@@ -0,0 +1,8 @@
+﻿using MetalVerseBackend.Models;
+using System;
+
+public class Inbox
+{
+    public Guid UserId { get; set; }
+    public List<Message> Messages { get; set; }
+}
