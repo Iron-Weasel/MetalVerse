b870953bb6611156cb2dd9625dc9aa4636646e00
malinapavel <malina.pavel@student.upt.ro>
2023-10-23 19:56:44 +0300

    Testing the UserController endpoints

--- MetalVerseBackend/Controllers/UserController.cs
+++ MetalVerseBackend/Controllers/UserController.cs
@@ -7,22 +7,49 @@ namespace MetalVerseBackend.Controllers
     [Route("users")]
     public class UserController : ControllerBase
     {
+        private List<User> _users = new List<User>();
+        public UserController()
+        {
+            _users.Add(new User()
+            {
+                Id = Guid.Parse("5d899972-6bfa-413d-bfa7-619fcfcd2706"),
+                FirstName = "A",
+                LastName = "B",
+                Email = "C",
+                Password = "D",
+                UserRole = UserRoles.BandMember
+            });
+
+            _users.Add(new User()
+            {
+                Id = Guid.Parse("67db0b44-fed7-49eb-9fa5-d291fe9cde64"),
+                FirstName = "X",
+                LastName = "Y",
+                Email = "Z",
+                Password = "V",
+                UserRole = UserRoles.StudioManager
+            });
+
+        }
         [HttpGet]
         public IActionResult GetUsers()
         {
-            return Ok();
+            var users = _users.ToList();
+            return Ok(users);
         }
 
         [HttpGet("{userId}")]
         public IActionResult GetUser(Guid userId)
         {
-            return Ok();
+            var user = _users.FirstOrDefault(x => x.Id == userId);
+            return Ok(user);
         }
 
         [HttpPost("add_user")]
         public IActionResult AdUser(User user)
         {
-            return Ok();
+            _users.Add(user);
+            return Ok(_users);
         }
     }
 }
