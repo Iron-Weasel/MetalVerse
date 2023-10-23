72bcf0d399fb3fdb4450df1d4093565e33607ef1
malinapavel <malina.pavel@student.upt.ro>
2023-10-23 20:39:28 +0300

    Created CommentController + PostController test

---
+++ MetalVerseBackend/Controllers/CommentController.cs
@@ -0,0 +1,29 @@
+﻿using MetalVerseBackend.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.Design;
+
+namespace MetalVerseBackend.Controllers
+{
+    [ApiController]
+    [Route("posts/{postId}")]
+    public class CommentController : ControllerBase
+    {
+        [HttpGet("comments")]
+        public IActionResult GetComments(Guid postId)
+        {
+            return Ok();
+        }
+
+        [HttpGet("comments/{commentId}")]
+        public IActionResult GetPost(Guid postId, Guid commentId)
+        {
+            return Ok();
+        }
+
+        [HttpPost("comments/add_comment")]
+        public IActionResult AddComment(Guid postId, Comment comment)
+        {
+            return Ok();
+        }
+    }
+}
--- MetalVerseBackend/Controllers/PostController.cs
+++ MetalVerseBackend/Controllers/PostController.cs
@@ -7,40 +7,45 @@ namespace MetalVerseBackend.Controllers
     [Route("posts")]
     public class PostController : ControllerBase
     {
-        [HttpGet]
-        public IActionResult GetPosts() 
+        private List<Post> _posts = new List<Post>();
+        public PostController()
         {
-            return Ok();
+            _posts.Add(new Post()
+            {
+                Id = Guid.Parse("5d899972-6bfa-413d-bfa7-619fcfcd2706"),
+                UserId = Guid.Parse("5d899972-6bfa-413d-bfa7-619fcfcd2706"),
+                Title = "B",
+                Description = "C"
+            });
+
+            _posts.Add(new Post()
+            {
+                Id = Guid.Parse("67db0b44-fed7-49eb-9fa5-d291fe9cde64"),
+                UserId = Guid.Parse("5d899972-6bfa-413d-bfa7-619fcfcd2706"),
+                Title = "X",
+                Description = "Y",
+                Views = 3,
+                RockOns = 4
+            });
+        }
+
+        [HttpGet]
+        public IActionResult GetPosts()
+        {
+            return Ok(_posts);
         }
 
         [HttpGet("{postId}")]
         public IActionResult GetPost(Guid postId)
         {
-            return Ok();
-        }
-
-        [HttpGet("{postId}/comments")]
-        public IActionResult GetComments(Guid postId)
-        {
-            return Ok();
-        }
-
-        [HttpGet("{postId}/comments/{commentId}")]
-        public IActionResult GetComment(Guid postId, Guid commentId)
-        {
-            return Ok();
+            return Ok(_posts.FirstOrDefault(x => x.Id == postId));
         }
 
         [HttpPost("add_post")]
         public IActionResult AddPost(Post post)
         {
-            return Ok();
-        }
-
-        [HttpPost("posts/{postId}/comments")]
-        public IActionResult AddCommen(Comment comment)
-        {
-            return Ok();
+            _posts.Add(post);
+            return Ok(_posts);
         }
     }
 }
