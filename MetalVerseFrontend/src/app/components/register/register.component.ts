f58ab344a2d93b0f357f12338dc303ffdd176bd1
malinapavel <malina.pavel@student.upt.ro>
2024-01-09 21:06:06 +0200

    Refined the validation forms for login and registration

--- MetalVerseFrontend/src/app/components/register/register.component.ts
+++ MetalVerseFrontend/src/app/components/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 import { User } from 'src/app/models/user';
 import { BackendHttpService } from 'src/app/services/backend.service';
 
@@ -10,19 +12,20 @@ import { BackendHttpService } from 'src/app/services/backend.service';
 export class RegisterComponent {
     selectedRole: string;
     isTermsSelected: boolean = false;
-    inputCheck: boolean = false;
-    routeCheck: boolean = false;
-    errorMessage: string = '';
-    @ViewChild('usernameInput') usernameInputRef: ElementRef;
-    @ViewChild('firstNameInput') firstNameInputRef: ElementRef;
-    @ViewChild('lastNameInput') lastNameInputRef: ElementRef;
-    @ViewChild('emailInput') emailInputRef: ElementRef;
-    @ViewChild('passwordInput') passwordInputRef: ElementRef;
-    @ViewChild('confirmInput') confirmInputRef: ElementRef;
+    isPasswordMatching: boolean = false;
+    credentials: User = {
+      firstName: '',
+      lastName: '',
+      email: '',
+      username:'', 
+      password:'',
+      confirmPassword: '',
+      userRole: ''
+    };
 
     private httpService: BackendHttpService;
 
-    constructor(httpService: BackendHttpService) { 
+    constructor(httpService: BackendHttpService, private router: Router) { 
       this.httpService  = httpService;
     }
 
@@ -31,43 +34,22 @@ export class RegisterComponent {
     }
 
     // clicking on "Register" will create a new user
-    //TO THINK FURTHER OF A BETTER VALIDATION
-    onSaveUser(): void {
-      const user: User = {
-        username: this.usernameInputRef.nativeElement.value,
-        firstName: this.firstNameInputRef.nativeElement.value,
-        lastName: this.lastNameInputRef.nativeElement.value,
-        email: this.emailInputRef.nativeElement.value,
-        password: this.passwordInputRef.nativeElement.value,
-        userRole: this.selectedRole
-      }  
-
-      const isEmpty = Object.values(user).some(x => x == null || x == '' || x == undefined);
-      if(isEmpty) {
-        this.errorMessage = "All fields are mandatory!";
-        this.inputCheck = false;
-      }
-      else {
-        const confirmPassWord = this.confirmInputRef.nativeElement.value;
-        if(confirmPassWord != this.passwordInputRef.nativeElement.value) {
-          this.errorMessage = "Passwords do not match!";
-          this.inputCheck = false;
+    public onSaveUser(form: NgForm): void {
+      this.isPasswordMatching = this.checkPasswordsMatch();
+      if(form.valid) {
+        if(this.isTermsSelected && this.isPasswordMatching) {
+          this.router.navigate(["/register-success"]);
+          this.httpService.saveUser(this.credentials).subscribe((data:User) => { });
         }
-        else this.inputCheck = true;
-      }
-
-      if(!this.isTermsSelected) {
-        this.errorMessage = "You must check this box!";
-        this.inputCheck = false;
-      }
-
-      if(this.isTermsSelected && this.inputCheck){
-        this.routeCheck = true;
-        this.httpService.saveUser(user).subscribe((data:User) => { });
+        else this.router.navigate(["/register"]);
       }
     }
 
     acceptTerms(): void {
       this.isTermsSelected = true;
+    }
+
+    private checkPasswordsMatch(): boolean {
+      return this.credentials.password === this.credentials.confirmPassword;
     }
   }
